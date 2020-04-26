552fd75831515a1f2bc79d53f2e5a72edbd8d078
Mikko Rajakangas <mikko.rajakangas@visma.com>
2020-04-26 13:57:00 +0300

    :art: courses into store

---
+++ dig4ta022-3004/store/courses.js
@@ -0,0 +1,24 @@
+export const state = () => ({
+	list: [
+		{
+			id: 1,
+			header: 'Otsikko 1',
+			description: 'Ensimmäisen kurssin kootut selitykset'
+		},
+		{
+			id: 2,
+			header: 'Otsikko 2',
+			description: 'Toisen kurssin kootut selitykset'
+		},
+		{
+			id: 3,
+			header: 'Otsikko 3',
+			description: 'Lorem ipsum loorum laarum'
+		},
+		{
+			id: 4,
+			header: 'Otsikko 4',
+			description: ''
+		}
+	]
+})
