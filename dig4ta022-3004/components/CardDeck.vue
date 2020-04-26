f9a4f7c69d8a809981d06468fc925b1d36451340
Mikko Rajakangas <mikko.rajakangas@visma.com>
2020-04-26 13:40:10 +0300

    :construction: init basic components and structure

---
+++ dig4ta022-3004/components/CardDeck.vue
@@ -0,0 +1,43 @@
+<template>
+	<b-card-group deck>
+		<div v-for="item in items" :key="item.id">
+			<card :item="item" />
+		</div>
+	</b-card-group>
+</template>
+
+<script>
+import Card from '~/components/Card.vue'
+export default {
+	name: 'card-deck',
+	components: {
+		Card
+	},
+	data() {
+		return {
+			items: [
+				{
+					id: 1,
+					header: 'Header',
+					description: 'This is a description'
+				},
+				{
+					id: 2,
+					header: 'Header',
+					description: 'This is a description'
+				},
+				{
+					id: 3,
+					header: 'Header',
+					description: 'This is a description'
+				},
+				{
+					id: 4,
+					header: 'Header',
+					description: 'This is a description'
+				}
+			]
+		}
+	}
+}
+</script>
---
+++ dig4ta022-3004/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+	<b-navbar sticky toggleable>
+		<b-navbar-brand>Oppia.io</b-navbar-brand>
+		<b-navbar-toggle target="navbar-toggle-collapse">
+			<template v-slot:default="{ expanded }">
+				<b-icon v-if="expanded" icon="x-circle" />
+				<b-icon v-else icon="grid" />
+			</template>
+		</b-navbar-toggle>
+		<b-collapse id="navbar-toggle-collapse" is-nav>
+			<b-navbar-nav class="ml-auto">
+				<b-nav-item>Linkki 1</b-nav-item>
+				<b-nav-item>Linkki 2</b-nav-item>
+				<b-nav-item>Linkki 3</b-nav-item>
+			</b-navbar-nav>
+		</b-collapse>
+	</b-navbar>
+</template>
+
+<script>
+export default {
+	name: 'navbar'
+}
+</script>
